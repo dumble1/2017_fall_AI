8f135e9253b794405ab9d12608e9aed7b751efff
unknown <philipp.roenchen@everymail.net>
2017-09-12 12:15:14 +0200

    Added documentation to the code. Added the option to cut off the zeros of the path returned by astar and reverse it

--- AI/DeliveryMan/R/astar.R
+++ AI/DeliveryMan/R/astar.R
@@ -1,31 +1,19 @@
+#In all our functions we treat x as the row dimension, y as the coloumn dimension
+
 myDM = function(roads,car,packages) {
-  hroads <- t(roads$hroads)
+  #transpose hroads and vroads to make dimensions fit
+  hroads <- t(roads$hroads) 
   vroads <- t(roads$vroads)
-  xdim <- ncol(hroads)
-  ydim <- nrow(vroads)
-  print(hroads)
-  print(vroads)
-  print(xdim)
-  print(ydim)
-  costarr <- array(0,dim = c(xdim,ydim,4)) #cost  matrix
+  xdim <- nrow(hroads) + 1
+  ydim <- ncol(hroads)
+  #Initialise costarr, which contains the edge costs to every node's neighbours
+  costarr <- array(0,dim = c(xdim,ydim,4))
   costarr[2:10,,1] = hroads #cost to the left
   costarr[,2:10,2] = vroads #cost below
   costarr[1:9,,3] = hroads #cost to the right
   costarr[,1:9,4] = vroads #cost above
-  print(costarr)
   print(astar(costarr,c(1,1),c(10,10)))
-  
-  #important comment
-  
 
-  # print(typeof(roads))
-  # hroads = matrix(unlist(roads[1]), ncol = 9, byrow = TRUE)
-  # vroads = roads[2]
-  # print(typeof(hroads))
-  # print(hroads)
-  # print(is.matrix(hroads))
-  # a <- ncol(hroads)
-  # print(a)
   return(car)
 }
 
@@ -42,26 +30,46 @@ hopcost = function(xdim,ydim,goal) {
 
 
 astar = function(costarr,source,goal) {
+  #Returns the shortest path (to be read backwards) from source to goul on a Manhattan grid. 
+  #Costarr stores the edge cost from each node to its neighbours (left,lower,right,upper)
+  #Source and goal are two-dimensional vectors of x- and y-coordinates
+  
+  #Local variables used
+  
+  #xdim, ydim: Store size of grid
   xdim = dim(costarr)[1]
   ydim = dim(costarr)[2]
+  #visiting: Stores the node that is expanded in the current iteration
+  visiting <- source
+  #visitedm: Stores all the nodes that have been expanded or are expanded in the current iteration
   visitedm <- matrix(FALSE, nrow = xdim, ncol = ydim)
-  #stores the best predecessor of all the vistied nodes
+  visitedm[source[1],source[2]] <- TRUE
+  #visited.previousarr: Stores the best predecessor of all the visited nodes.
+  # Used later to reconstruct the optimal path
+  visited.previousarr <- array(0,dim=c(xdim,ydim,2))
+  #visited.costm: Stores the cost of the optimal path to all expanded nodes
   visited.costm <- matrix(Inf,xdim,ydim)
   visited.costm[source[1],source[2]] <- 0
-  visited.previousarr <- array(0,dim=c(xdim,ydim,2))
-  visiting <- source
+  
+  #hopcostm: Stores the shortest number of hops from every node to the goal
   hopcostm <- hopcost(xdim,ydim,goal)
-  #stores the expected cost to the nodes in the frontier
-  frontier.costm <- matrix(Inf,xdim,ydim)
-  #stores the best predecessor of the nodes in the frontier
-  frontier.previousarr <- array(0,dim=c(xdim,ydim,2))
-  #frontier.pathm[source] <- list("cost" = 0, "path" = list(source))
+  #frontier.expectedcostm: stores the expected cost from source to goal
+  # if the route over the node in the frontier is taken
+  # Entry is infinitiy iff the node is not in the frontier
+  frontier.expectedcostm <- matrix(Inf,xdim,ydim)
+  #Stores the expected predecessor on the path from source to goal over the node in the frontier
+  frontier.expectedpreviousarr <- array(0,dim=c(xdim,ydim,2))
+  #(As long as a node is the frontier, its expected cost and its expected predecessor might be updated in any interation)
+  
+ 
   
   while(visitedm[goal[1],goal[2]] == FALSE) {
     has.leftneigh <- !(visiting[1] == 1)
     has.lowerneigh <- !(visiting[2] == 1)
     has.rightneigh <- !(visiting[1] == xdim)
     has.upperneigh <- !(visiting[2] == ydim)
+    
+    #Identify coordinates and expected path cost of neighbours of current node
     if(has.leftneigh) {
       leftneigh <- visiting - c(1,0)
       left.expectedcost <- visited.costm[visiting[1],visiting[2]] + costarr[visiting[1],visiting[2],1] + hopcostm[leftneigh[1],leftneigh[2]]
@@ -79,44 +87,42 @@ astar = function(costarr,source,goal) {
       upper.expectedcost <- visited.costm[visiting[1],visiting[2]] + costarr[visiting[1],visiting[2],4] + hopcostm[upperneigh[1],upperneigh[2]]
     }
 
+    #If the neighbours have not been expanded yet, add them to the frontier (if they already are in the frontier,
+    #update their expected costs and predecessors if appropiate)
     if(has.leftneigh == TRUE && visitedm[leftneigh[1],leftneigh[2]] == FALSE &&
-       left.expectedcost < frontier.costm[leftneigh[1],leftneigh[2]]) {
-      frontier.costm[leftneigh[1],leftneigh[2]] <- left.expectedcost
-      frontier.previousarr[leftneigh[1],leftneigh[2],] <- visiting
+       left.expectedcost < frontier.expectedcostm[leftneigh[1],leftneigh[2]]) {
+      frontier.expectedcostm[leftneigh[1],leftneigh[2]] <- left.expectedcost
+      frontier.expectedpreviousarr[leftneigh[1],leftneigh[2],] <- visiting
     }
     if(has.lowerneigh == TRUE && visitedm[lowerneigh[1],lowerneigh[2]] == FALSE &&
-       lower.expectedcost < frontier.costm[lowerneigh[1],lowerneigh[2]]) {
-      frontier.costm[lowerneigh[1],lowerneigh[2]] <- lower.expectedcost
-      frontier.previousarr[lowerneigh[1],lowerneigh[2],] <- visiting
+       lower.expectedcost < frontier.expectedcostm[lowerneigh[1],lowerneigh[2]]) {
+      frontier.expectedcostm[lowerneigh[1],lowerneigh[2]] <- lower.expectedcost
+      frontier.expectedpreviousarr[lowerneigh[1],lowerneigh[2],] <- visiting
     }
     if(has.rightneigh == TRUE && visitedm[rightneigh[1],rightneigh[2]] == FALSE &&
-       right.expectedcost < frontier.costm[rightneigh[1],rightneigh[2]]) {
-      frontier.costm[rightneigh[1],rightneigh[2]] <- right.expectedcost
-      frontier.previousarr[rightneigh[1],rightneigh[2],] <- visiting
+       right.expectedcost < frontier.expectedcostm[rightneigh[1],rightneigh[2]]) {
+      frontier.expectedcostm[rightneigh[1],rightneigh[2]] <- right.expectedcost
+      frontier.expectedpreviousarr[rightneigh[1],rightneigh[2],] <- visiting
     }
     if(has.upperneigh == TRUE && visitedm[upperneigh[1],upperneigh[2]] == FALSE &&
-       upper.expectedcost < frontier.costm[upperneigh[1],upperneigh[2]]) {
-      frontier.costm[upperneigh[1],upperneigh[2]] <- upper.expectedcost
-      frontier.previousarr[upperneigh[1],upperneigh[2],] <- visiting
+       upper.expectedcost < frontier.expectedcostm[upperneigh[1],upperneigh[2]]) {
+      frontier.expectedcostm[upperneigh[1],upperneigh[2]] <- upper.expectedcost
+      frontier.expectedpreviousarr[upperneigh[1],upperneigh[2],] <- visiting
     }
     
-    frontier.costm[visiting[1],visiting[2]] <- Inf
+    frontier.expectedcostm[visiting[1],visiting[2]] <- Inf #delete expanded node from frontier
+    
+    #choose one of the nodes with minimal expected cost in the frontier to be expanded next
+    visiting <- which(frontier.expectedcostm == min(frontier.expectedcostm), arr.ind = TRUE)[1,]
     visitedm[visiting[1],visiting[2]] <- TRUE
     
-    #print(frontier.costm)
-    #print(which(frontier.costm == min(frontier.costm), arr.ind = TRUE)[1,])
-    visiting <- which(frontier.costm == min(frontier.costm), arr.ind = TRUE)[1,]
-    visited.costm[visiting[1],visiting[2]] <- frontier.costm[visiting[1],visiting[2]] - hopcostm[visiting[1],visiting[2]]
-    visited.previousarr[visiting[1],visiting[2],] <- frontier.previousarr[visiting[1],visiting[2],]
-    
-    # print("visiting")
-    # print(visiting)
-    # print("frontier.costm")
-    # print(frontier.costm)
+    #By the properties of A*, we have found the optimal path to the node that is expanded
+    visited.costm[visiting[1],visiting[2]] <- frontier.expectedcostm[visiting[1],visiting[2]] - hopcostm[visiting[1],visiting[2]]
+    visited.previousarr[visiting[1],visiting[2],] <- frontier.expectedpreviousarr[visiting[1],visiting[2],]
     
   }
   
-  print(visited.costm)
+  #Reconstruct path starting at the goal
   pathm <- matrix(0,xdim*ydim,2)
   pathm[1,] = goal
   where.onpath = goal
@@ -126,14 +132,21 @@ astar = function(costarr,source,goal) {
     pathm[i,] <- where.onpath
     i <- i+1
   }
+  # #Activate if you want to cut off zeros and reverse the path
+  # pathx <- pathm[,1]
+  # pathy <- pathm[,2]
+  # pathx <- pathx[which(pathx != 0)]
+  # pathy <- pathy[which(pathy != 0)]
+  # pathm <- cbind(pathx,pathy)
+  # pathm <- apply(pathm, 2, rev)
+  
   return(pathm)
 }
 
-extract.nextmove = function(pathm) {
-  
-}
 
 stupid.randomm = function(nrows,ncols,maxvalue) {
+  #Creates a matrix of size nrows x ncols with random integers in {1,...,maxvalue}
+  #Probably slow
   a <- matrix(1,nrow=nrows,ncol=ncols)
   for (i in 1:nrows) {
     for (j in 1:ncols) {
@@ -144,15 +157,21 @@ stupid.randomm = function(nrows,ncols,maxvalue) {
 }
 
 test = function(nrows) {
-  hroads <- stupid.randomm(nrows,nrows-1,5)
-  vroads <- stupid.randomm(nrows-1,nrows,5)
+  #Runs A* on a grid of size nrows x nrows with random edge costs between 1 and 5
+  #to obtain the shortest path between the points (1,1) and (nrows,nrows)
+  
+  #randomly generate edges and print them
+  hroads <- stupid.randomm(nrows-1,nrows,5)
+  vroads <- stupid.randomm(nrows,nrows-1,5)
   print(hroads)
   print(vroads)
-  costarr <- array(0,dim = c(nrows,nrows,4)) #cost  matrix
+  
+  #Initialise cost array
+  costarr <- array(0,dim = c(nrows,nrows,4))
   costarr[2:nrows,,1] = hroads #cost to the left
   costarr[,2:nrows,2] = vroads #cost below
   costarr[1:(nrows-1),,3] = hroads #cost to the right
   costarr[,1:(nrows-1),4] = vroads #cost above
-  #print(costarr)
+
   print(astar(costarr,c(1,1),c(nrows,nrows)))
 }
